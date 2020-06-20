0b909808841d9968d57dccc1d437886924094157
unknown <andejt1@student.op.ac.nz>
2020-06-20 13:20:31 +1200

    initial

---
+++ Project Dark - Main/Assets/Scripts/playerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+      
+{
+    static Animator anim;
+    float negZ = -1, negX = -1, posZ = 1, posX =1;
+    float maxHealth = 1200;
+    float currentCondition;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        currentCondition = maxHealth;
+    }
+
+    
+    void Update()
+    {
+        //Kill player debug//
+        if (Input.GetKey("l"))
+        {
+            anim.SetBool("isDead", true);
+        }
+        else
+        {
+            anim.SetBool("isDead", false);
+        }
+
+        //Walking Animation conditions//
+        if (Input.GetKey("w"))
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("moveZ", posZ);
+        }
+        else if(Input.GetKey("s"))
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("moveZ", negZ);
+        }
+        else
+        {
+            anim.SetFloat("moveZ", 0);
+        }
+
+
+        if (Input.GetKey("a"))
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("moveX", negX);
+        }
+        else if (Input.GetKey("d"))
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("moveX", posX);
+        }
+        else
+        {
+           //anim.SetBool("isWalking", false);
+           anim.SetFloat("moveX", 0);
+        }
+    }
+}
---
+++ Project Dark - Main/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/PlayerMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouse : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -60f, 60f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
